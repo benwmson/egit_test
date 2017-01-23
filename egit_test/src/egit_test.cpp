1314e7628cd857c2539589ffdadb93858d5df1a6
Ben Williamson <benjamin.wmson@gmail.com>
2017-01-23 15:49:23 -0700

    Initial commit.

---
+++ egit_test/src/egit_test.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : egit_test.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "WORDS!!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
